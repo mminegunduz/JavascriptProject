6b4432e5adea2f9ca3b9a9f6ee5762b3df784abc
Mine <142803161+mminegunduz@users.noreply.github.com>
2024-02-06 15:56:44 +0300

    Dictionary
    ![dictionary](https://github.com/mminegunduz/JavascriptProject/assets/142803161/8893a8b2-43a5-4beb-af9b-dd70e1a63f5f)

---
+++ English-English Dictionary/mine.js
@@ -0,0 +1,27 @@
+const wordInput=document.getElementById('word');
+const button=document.querySelector('button');
+const translatedDiv=document.getElementById('translatedDiv');
+const tittle=document.getElementById('tittle');
+const meaning=document.getElementById('meaning');
+const audio=document.getElementById('audio');
+
+async function fetchApi() {
+  translatedDiv.style.display= `none`;
+  const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`
+  const result= await fetch(url).then((res)=> res.json());
+  console.log(result);
+  if(result.title){
+
+  }
+  else{
+    translatedDiv.style.display= `block`;
+    tittle.textContent=result[0].word;
+    meaning.textContent=result[0].meanings[0].definitions[0].definition;
+    audio.src=result[0].phonetics[0].audio;
+  }
+}
+
+
+
+button.addEventListener('click',fetchApi)
+
